f04489f7e76a6095d500a76f59a80dc50062ebe1
Kang <rkdalsgur032@unist.ac.kr>
2023-10-05 12:33:08 +0900

    middleware http logger

--- apps/nest-app-1/src/app.module.ts
+++ apps/nest-app-1/src/app.module.ts
@@ -1,9 +1,14 @@
-import { Logger, Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [UserModule],
   controllers: [],
   providers: [Logger],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
